d7113b4192ac8f4f304e74fff57865b032501936
F0ur <4@linkedtune.cn>
2017-03-10 18:37:37 +0800

    更新至0.2, 提示内网地址

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
+	"os"
 )
 
 const (
@@ -14,8 +16,21 @@ const (
 func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(WallPaperPath))))
 
-	log.Printf("WallPaper Server Ready Start\n")
-	log.Printf("Use 127.0.0.1%s To Visit\n", ListenPort)
+	log.Printf("服务端即将启动\n")
+	log.Printf("电脑测试地址：http://127.0.0.1%s\n", ListenPort)
+
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatalln("Network interface Error:", err.Error())
+		os.Exit(1)
+	}
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && ipnet.IP.IsGlobalUnicast() {
+			if ipnet.IP.To4() != nil {
+				log.Printf("PS4访问地址：http://%s%s", ipnet.IP.String(), ListenPort)
+			}
+		}
+	}
 
 	if err := http.ListenAndServe(ListenPort, nil); err != nil {
 		log.Fatalln("WallPaper Server Start Error: ", err)
